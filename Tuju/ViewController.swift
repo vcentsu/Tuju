5ee44d52736e80154370ac255ef946e6008d33ce
vcentsu <vincenttius.sutanto@gmail.com>
2022-10-11 16:16:03 +0700

    add googlemaps

--- Tuju/ViewController.swift
+++ Tuju/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        GMSServices.provideAPIKey("AIzaSyC-gr-6ddyZ_XMEtf7plw4Rlpk61Syo30o")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
 
