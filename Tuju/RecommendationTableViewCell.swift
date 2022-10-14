cc302219db000e7fde813caa905a9b4fa7993358
vcentsu <vincenttius.sutanto@gmail.com>
2022-10-14 15:25:53 +0700

    update asal vc

---
+++ Tuju/RecommendationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecommendationTableViewCell.swift
+//  Tuju
+//
+//  Created by Vincentius Sutanto on 14/10/22.
+//
+
+import UIKit
+
+class RecommendationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
