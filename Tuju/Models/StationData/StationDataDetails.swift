357e5f33452aceee4c1d6fa08fdbd8e6693fc99e
eldwinanthony <eldwinanthony@gmail.com>
2022-10-21 17:35:28 +0700

    Add station and coordinate data model

---
+++ Tuju/Models/StationData/StationDataDetails.swift
@@ -0,0 +1,48 @@
+//
+//  StationDataDetails.swift
+//  Tuju
+//
+//  Created by Eldwin Anthony on 21/10/22.
+//
+
+import Foundation
+import UIKit
+
+var Hijau: [HijauContent] = [
+    HijauContent(namaStasiun: "Pondok Ranji", latitude: -6.2761221112337555, longitude: 106.74492972581243),
+    HijauContent(namaStasiun: "Kebayoran", latitude: -6.237100410964969, longitude: 106.78254607194879),
+    HijauContent(namaStasiun: "Palmerah", latitude: -6.2071537130553835, longitude: 106.79744762581176),
+    HijauContent(namaStasiun: "Tanah Abang", latitude: -6.185360771522169, longitude: 106.81099307183692)
+]
+
+var BiruKiri: [BiruKiriContent] = [
+    BiruKiriContent(namaStasiun: "Karet", latitude: -6.200547009851408, longitude: 106.81575013745739),
+    BiruKiriContent(namaStasiun: "Sudirman", latitude: -6.201969746392874, longitude: 106.82321755464723),
+    BiruKiriContent(namaStasiun: "Manggarai", latitude: -6.209675277806892, longitude: 106.85025771231817)
+]
+
+var MerahAtas: [MerahAtasContent] = [
+    MerahAtasContent(namaStasiun: "Manggarai", latitude: -6.209675277806892, longitude: 106.8413169411535),
+    MerahAtasContent(namaStasiun: "Cikini", latitude: -6.198300308762655, longitude: 106.82321755464723),
+    MerahAtasContent(namaStasiun: "Gondangdia", latitude: -6.185873336384879, longitude: 106.83268145464706),
+    MerahAtasContent(namaStasiun: "Gambir", latitude: -6.176412531283551, longitude: 106.83059726814066),
+    MerahAtasContent(namaStasiun: "Juanda", latitude: -6.166508260411192, longitude: 106.8305051834823),
+    MerahAtasContent(namaStasiun: "Sawah Besar", latitude: -6.160505991127654, longitude: 106.82763926814044),
+    MerahAtasContent(namaStasiun: "Mangga Besar", latitude: -6.149582587811262, longitude: 106.82694382396323)
+]
+
+var BiruKanan: [BiruKananContent] = [
+    BiruKananContent(namaStasiun: "Manggarai", latitude: -6.209675277806892, longitude: 106.85025771231817),
+    BiruKananContent(namaStasiun: "Matraman", latitude: -6.21201398124878, longitude: 106.86029933930554),
+    BiruKananContent(namaStasiun: "Jatinegara", latitude: -6.214925343036553, longitude: 106.87030561231829)
+]
+
+var MerahBawah: [MerahBawahContent] = [
+    MerahBawahContent(namaStasiun: "Manggarai", latitude: -6.209675277806892, longitude: 106.8413169411535),
+    MerahBawahContent(namaStasiun: "Tebet", latitude: 6.225356018591745, longitude: 106.85899364115377),
+    MerahBawahContent(namaStasiun: "Cawang", latitude: -6.242260930137949, longitude: 106.85878466814137),
+    MerahBawahContent(namaStasiun: "Duren Kalibata", latitude: -6.25518778602565, longitude: 106.8550798123186),
+    MerahBawahContent(namaStasiun: "Pasar Minggu Baru", latitude: -6.262559664090336, longitude: 106.85184385464775),
+    MerahBawahContent(namaStasiun: "Pasar Minggu", latitude: -6.284010879311879, longitude: 106.84451421047072),
+    MerahBawahContent(namaStasiun: "Tanjung Barat", latitude: -6.3077286259516985, longitude: 106.83887311601525)
+]
