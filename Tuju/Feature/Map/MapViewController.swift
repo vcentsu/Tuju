3ffaa36ff0af8f8fbbfb81083246cf3e1bcaa9e2
eldwinanthony <eldwinanthony@gmail.com>
2022-11-08 16:26:23 +0700

    Enter and exit geofence done

--- Tuju/Feature/Map/MapViewController.swift
+++ Tuju/Feature/Map/MapViewController.swift
@@ -10,8 +10,9 @@ import MapKit
 import FloatingPanel
 import GoogleMaps
 import CoreLocation
+import AVFoundation
 
-class MapViewController: UIViewController, GMSMapViewDelegate, PanelViewControllerDelegate {
+class MapViewController: UIViewController, GMSMapViewDelegate{
     
     let mapView = GMSMapView(frame: .zero)
     
@@ -33,43 +34,22 @@ class MapViewController: UIViewController, GMSMapViewDelegate, PanelViewControll
         //view.addSubview(mapView)
         
         // Do any additional setup after loading the view.
-        self.manager.delegate = self
-        self.manager.requestWhenInUseAuthorization()
-        self.manager.startUpdatingLocation()
-        self.mapView.delegate = self
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
+        mapView.delegate = self
         
-
-        // Set Initial Camera Position
-        let camera = GMSCameraPosition.camera(
-            withLatitude: coordinateLive.latitude,
-            longitude: coordinateLive.longitude,
-            zoom: 14.0
-        )
-
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        let geoFenceRegion:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(-6.209675277806892, 106.85025771231817), radius: 100, identifier: "Manggarai")
+        manager.startMonitoring(for: geoFenceRegion)
 
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: coordinateLive.latitude, longitude: coordinateLive.longitude)
-        let markerCenter = CLLocationCoordinate2D(latitude: coordinateLive.latitude, longitude: coordinateLive.longitude)
-        marker.title = "Stasiun Manggarai"
         marker.map = mapView
-        
-        let markerCircle = GMSCircle(position: markerCenter, radius: 200)
-        markerCircle.map = mapView
-        markerCircle.fillColor = UIColor(red: 0.0, green: 0, blue: 0.7, alpha: 0.2)
-        markerCircle.strokeColor = .blue
-        markerCircle.strokeWidth = 3
         
         mapView.delegate = self
         self.view = mapView
         
-        print(GMSServices.openSourceLicenseInfo())
-        
-        manager.requestWhenInUseAuthorization()
-//        self.mapView.travelMode = .cycling
 
         let panelVC = Tuju.PanelViewController()
-        panelVC.delegate = self
         
         panel.set(contentViewController: panelVC)
         panel.addPanel(toParent: self)
@@ -81,74 +61,43 @@ class MapViewController: UIViewController, GMSMapViewDelegate, PanelViewControll
         circle.fillColor = UIColor(red: 0.7, green: 0, blue: 0, alpha: 0.2)
         circle.strokeColor = .red
         circle.strokeWidth = 3
-//        self.view = mapView
     }
     
-    //    func startNav() {
-    //      var destinations = [GMSNavigationWaypoint]()
-    //      destinations.append(GMSNavigationWaypoint.init(placeID: "ChIJmQ6sHHH0aS4R2Kc4sEiEyUc",
-    //                                                     title: "Stasiun Manggarai")!)
-    //      destinations.append(GMSNavigationWaypoint.init(placeID:"ChIJt_uvMxX0aS4RgasvyQI7DJU",
-    //                                                     title:"Stasiun Cikini")!)
-    //
-    //      mapView.navigator?.setDestinations(destinations) { routeStatus in
-    //        self.mapView.navigator?.isGuidanceActive = true
-    //        self.mapView.locationSimulator?.simulateLocationsAlongExistingRoute()
-    //        self.mapView.cameraMode = .following
-    //      }
-    //    }
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entered: \(region.identifier)")
+        AudioServicesPlaySystemSound(systemSoundID)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Exited: \(region.identifier)")
+    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         mapView.frame = view.bounds
     }
-    
-
-    
-    func PanelViewController(_ vc: PanelViewController, didSelectLocationWith coordinates: CLLocationCoordinate2D?) {
-        
-        guard let coordinates = coordinates else {
-            return
-        }
-        
-        panel.move(to: .half, animated: true)
-        
-        print("UTAMA! \(coordinates)")
-        
-        let camera = GMSCameraPosition.camera(
-                    withLatitude: coordinates.latitude,
-                    longitude: coordinates.longitude,
-                    zoom: 14.0
-        )
-        
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
-        
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
-        marker.title = locations.description
-        marker.snippet = "Australia"
-        marker.map = mapView
-        
-        self.view = mapView
-    }
 }
 
 extension MapViewController: CLLocationManagerDelegate{
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.first else {
-            return
+        
+        guard let currentlocValue: CLLocationCoordinate2D = manager.location?.coordinate else {return}
+        let newLocation = locations.last
+        let camera = GMSCameraPosition.camera(withLatitude: (newLocation?.coordinate.latitude)!, longitude: (newLocation?.coordinate.longitude)!, zoom: 15.0)
+        self.mapView.animate(to: camera)
+        let lat  = (newLocation?.coordinate.latitude)! // get current location latitude
+        let long = (newLocation?.coordinate.longitude)! //get current location longitude
+        
+        marker.position = CLLocationCoordinate2DMake(lat, long)
+        
+        DispatchQueue.main.async {
+            self.marker.map = self.mapView  // Setting marker on mapview in main thread.
+            }
+        
+        for currentLocation in locations{
+            print("\(index): \(locations)")
+            //"0: [locations]"
         }
-        
-        let coordinateLive = location.coordinate
-        
-        mapView.isMyLocationEnabled = true
-        mapView.settings.myLocationButton = true
-        
-        marker.position = coordinateLive
-        marker.map = mapView
-    
-        print("License: \n\n\(GMSServices.openSourceLicenseInfo())")
     }
 }
 
---
+++ Tuju/Utilities/SoundNotification.swift
@@ -0,0 +1,13 @@
+//
+//  SoundNotification.swift
+//  Tuju
+//
+//  Created by Eldwin Anthony on 08/11/22.
+//
+
+import Foundation
+import AVFoundation
+
+let systemSoundID: SystemSoundID = 1016
+
+
